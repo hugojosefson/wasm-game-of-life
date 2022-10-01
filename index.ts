bd23973d88d2f266abc69a7bdd0666fcf9823921
Hugo Josefson <hugo@josefson.org>
2022-10-01 18:29:38 +0200

    feat: render in rust

--- index.ts
+++ index.ts
@@ -1,53 +1,30 @@
 // @deno-types="../pkg/wasm_game_of_life.d.ts"
-import { Cell, getUint8Memory0, Universe } from "./pkg/wasm_game_of_life.js";
+import { Universe } from "./pkg/wasm_game_of_life.js";
 
 const universe = Universe.new();
-
-const width = universe.width;
-const height = universe.height;
-const cells = new Uint8Array(
-  getUint8Memory0().buffer,
-  universe.cellsPointer,
-  width * height,
-);
 
 const CURSOR_TOP_LEFT = "\x1b[1;1H";
 const CURSOR_HIDE = "\x1b[?25l";
 const CURSOR_SHOW = "\x1b[?25h";
 
-const cellOf = (cell: Cell): string => cell === Cell.Alive ? "◼" : " ";
-
-const getRow = (startOffset: number) =>
-  Array.from(
-    { length: width },
-    (_, x) => cellOf(cells[startOffset + x]),
-  )
-    .join("");
-
 let done = false;
 
-function render() {
-  return Array.from(
-    { length: height },
-    (_, y) => getRow(y * width),
-  ).join("\n");
-}
-
-function next() {
+function anotherTickPlease(): void {
   universe.tick();
+  console.log(CURSOR_TOP_LEFT + universe.render());
 
-  const output = CURSOR_TOP_LEFT + /*universe.*/ render();
-  console.log(output);
-
-  if (!done) {
-    return setTimeout(next);
+  if (done) {
+    return;
   }
-  console.log(CURSOR_SHOW);
+  setTimeout(anotherTickPlease);
 }
 
 Deno.addSignalListener("SIGINT", () => {
   done = true;
 });
+addEventListener("unload", () => {
+  console.log(CURSOR_SHOW);
+});
 console.log(CURSOR_HIDE);
 console.clear();
-next();
+anotherTickPlease();
